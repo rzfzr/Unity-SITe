103e83cfd31c2df9dc4722615bc90cd1749d5d72
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2017-09-21 23:25:49 -0300

    Started movement

---
+++ SITeXProject/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 10;
+	//Rigidbody rb;
+
+
+	void Start() {
+		InitialPosition(); //handle position
+
+		//rb = GetComponent<Rigidbody>();
+	}
+
+	void InitialPosition() {
+		transform.position = new Vector3(0,0,0);
+		print("Centralizado");
+	}
+
+
+
+	void Update() {
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0,Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+		//rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0,Input.GetAxis("Vertical") * speed * Time.deltaTime));
+		//if (Input.GetKey(KeyCode.RightArrow)) {
+		//	transform.position += new Vector3(1 * speed * Time.deltaTime,0,0);
+		//}
+		//if (Input.GetKey(KeyCode.LeftArrow)) {
+		//	transform.position += new Vector3(-1 * speed * Time.deltaTime,0,0);
+		//}
+		//if (Input.GetKey(KeyCode.UpArrow)) {
+		//	transform.position += new Vector3(0,0,1 * speed * Time.deltaTime);
+		//}
+		//if (Input.GetKey(KeyCode.DownArrow)) {
+		//	transform.position += new Vector3(0,0,-1 * speed * Time.deltaTime);
+		//}
+
+	}
+}
