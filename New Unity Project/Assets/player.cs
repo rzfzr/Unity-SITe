5b22b3e464495909a0070107398c41342aafe2fc
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2017-10-16 19:21:32 -0200

    Test

---
+++ New Unity Project/Assets/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+
+
+
+	public GameObject go;
+	void Start() {
+
+	}
+
+	public float speed = 10;
+	void Update() {
+
+
+		//if (Input.GetKey(KeyCode.Space)) {
+		GameObject temp = Instantiate(go);
+		temp.GetComponent<Renderer>().material.color = new Color(Random.Range(0,1f),Random.Range(0,1f),Random.Range(0,1f));
+		//}
+
+
+
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0,Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+
+	}
+}
