afdb64e4e1bcad5c7f9f777f793c63397c9dce93
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2017-10-17 00:54:34 -0200

    Added bounciness

---
+++ Test/Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	//[Range(-20,20)]
+	//[SerializeField]
+	public float speed;
+
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+
+		//GetComponent<Rigidbody>().AddForce(new Vector3(speed * Input.GetAxis("Horizontal"),0,speed * Input.GetAxis("Vertical")));
+
+		transform.position += new Vector3(speed * Time.deltaTime * Input.GetAxis("Horizontal"),0,speed * Time.deltaTime * Input.GetAxis("Vertical"));
+
+
+
+		//print(Input.GetAxis("Horizontal"));
+
+
+		print(transform.position.x);
+
+	}
+
+
+}
